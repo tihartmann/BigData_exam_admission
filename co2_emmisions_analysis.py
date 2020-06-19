0b9454a016c5dcdaf6365899a76a4af4d20ace26
Timo <timo.hartmann@gesis.org>
2020-06-19 18:43:18 +0200

    added kmeans clustering

--- co2_emmisions_analysis.py
+++ co2_emmisions_analysis.py
@@ -1,9 +1,10 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 import pyspark.sql.functions as F
+from pyspark.sql.types import FloatType
 from functools import reduce
-import math
-
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.feature import VectorAssembler
 
 def preprocess_data(df):
     """
@@ -26,15 +27,38 @@ def preprocess_data(df):
     # per column, but I don't think it makes a lot of sense, since the variations are high
     df = df.na.drop("all", subset=[col for col in df.schema.names[2:]])
 
-    # 2. Using the fill function to impute missing values
+    # 2. Using the fill function to impute missing values with the mean of the country
     year_cols = df.columns[2:]
     df = df.withColumn('mean', (sum([df[i] for i in year_cols]) / len(year_cols))).na.fill('mean', subset=df.columns[:-1]).select(df.columns[:2] + year_cols)
 
+    # drop all years but 2004 and 2014, because they are not needed for the analysis
+    df = df.select(df.columns[:2] + ["2004", "2014"])
+
     return df
 
+def compute_emission_change(row):
+    """
+    For each country (i.e., for each row) compute the change between each year  
+    """
+    change = (row["2014"] - row["2004"]) / row["2004"]
+    return change
+
+def compute_k_means(df, k):
+    va = VectorAssembler().setInputCols(["change"]).setOutputCol("features")
+
+    new_df = va.setHandleInvalid("skip").transform(df)
+    kmeans = KMeans().setK(k).setSeed(1)
+    
+    model = kmeans.fit(new_df)
+
+
+    # add the cluster predictions to the data frame
+    transformed = model.transform(new_df)
+    return transformed
+
+
+
 def main():
-    print("HI")
-    # set configuration
     
 
     # 1. import the data
@@ -43,7 +67,15 @@ def main():
     #co2_data.write.option("header", "true").csv("test")
     #co2_data.show(5)
 
+    # compute change
+    compute_change_udf = F.udf(compute_emission_change, FloatType())
+    co2_data = co2_data.withColumn("change", compute_change_udf(F.struct([co2_data[x] for x in co2_data.columns])))
+    
+    co2_data = compute_k_means(co2_data, k=6)
+    co2_data.show(50)
+
 if __name__ == "__main__":
+    # set configuration and context
     conf = SparkConf().setAppName("ExamAdmission")
     sc = SparkContext(conf=conf)
     sc.setLogLevel("ERROR")
